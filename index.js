511e5918ddb4502ea1b9c71022215bf7683d569b
breannams <breannamarie_s@hotmail.com>
2021-07-06 21:39:45 -0400

    commit tree c3fca73b1c494d87d434dcfbea20024a26135f0d

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solution here
+function findMatching(drivers, name){
+return drivers.filter(driver =>
+ driver.toUpperCase() === name.toUpperCase())
+}
+
+
+function fuzzyMatch(drivers, letter){
+return drivers.filter(driver =>
+    driver[0] === letter[0])
+
+}
+
+
+function matchName(drivers, name){
+return drivers.filter(driver => driver.name === name)
+}
